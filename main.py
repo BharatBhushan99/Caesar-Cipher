a126c5501526b77ac7ca10d91fd9f0525bed5b11
bbhushan99 <100056888+BharatBhushan99@users.noreply.github.com>
2022-04-13 07:48:09 +0000

    Refactored the code

--- main.py
+++ main.py
@@ -6,37 +6,24 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 message = input("Type your message:\n")
 shift = int(input("Type the shift number:\n"))
 
-def ecncrypt(text, shift):
+def caesar(text, shift, direction):
 
-  encrypted_message = ""
-  
-  for letter in text:
-    current_index = alphabet.index(letter)
-    new_index = current_index + shift
-
-    if new_index > 25:
-      new_index = (new_index % 25) - 1
-
-    encrypted_message += alphabet[new_index]
-    
-  print(f"Here's your encrypted result: {encrypted_message}")
-
-def decrypt(text, shift):
-
-  decrypted_message = ""
+  message=""
 
   for letter in text:
     current_index = alphabet.index(letter)
-    new_index = current_index - shift
 
-    decrypted_message += alphabet[new_index]
+    if direction == 'encode':
+      new_index = current_index + shift
 
-  print(f"Here's your decrypted message: {decrypted_message}")
+      if new_index > 25:
+        new_index = (new_index % 25) - 1
+
+    elif direction == 'decode':
+      new_index = current_index - shift
+
+    message += alphabet[new_index]
+
+  print(f"Here's your {direction}d result: {message}")
   
-if direction == 'encode':
-  ecncrypt(message, shift)
-elif direction == 'decode':
-  decrypt(message, shift)
-else:
-  print("Incorrect input.")
-  
+caesar(message, shift, direction)
