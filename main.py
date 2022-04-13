24393515b8be70c24c928820d464de490c84afcd
bbhushan99 <100056888+BharatBhushan99@users.noreply.github.com>
2022-04-13 06:47:30 +0000

    Add encryption functionality.

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 message = input("Type your message:\n")
 shift = int(input("Type the shift number:\n"))
 
+def ecncrypt(text, shift):
+
+  encrypted_message = ""
+  
+  for letter in text:
+    current_index = alphabet.index(letter)
+    new_index = current_index + shift
+
+    if new_index > 25:
+      new_index = (new_index % 25) - 1
+
+    encrypted_message += alphabet[new_index]
+    
+  print(f"Here's your encrypted result: {encrypted_message}")
+    
+if direction == 'encode':
+  ecncrypt(message, shift)
+
+  
