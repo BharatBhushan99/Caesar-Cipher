1316fdb4adf15c5263c6ae9e2dbf15881dbd5b7b
bbhushan99 <100056888+BharatBhushan99@users.noreply.github.com>
2022-04-14 03:40:29 +0000

    Added graphic logo and looping functionality.

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
+import graphics
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n")
-shift = int(input("Type the shift number:\n"))
 
 def caesar(text, shift, direction):
 
   message=""
 
   for letter in text:
-    current_index = alphabet.index(letter)
 
-    if direction == 'encode':
-      new_index = current_index + shift
-
-      if new_index > 25:
-        new_index = (new_index % 25) - 1
-
-    elif direction == 'decode':
-      new_index = current_index - shift
-
-    message += alphabet[new_index]
-
+    if letter in alphabet:
+      current_index = alphabet.index(letter)
+    
+      if direction == 'encode':
+        new_index = current_index + shift
+    
+        if new_index > 25:
+          new_index = (new_index % 25) - 1
+    
+      elif direction == 'decode':
+        new_index = current_index - shift
+    
+      letter = alphabet[new_index]
+   
+    message += letter
+      
   print(f"Here's your {direction}d result: {message}")
   
-caesar(message, shift, direction)
+keepLooping = True
+
+print(graphics.logo)
+
+while keepLooping:
+  
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  message = input("Type your message:\n")
+  shift = int(input("Type the shift number:\n"))
+
+  caesar(message, shift, direction)
+
+  choice = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if choice == 'no':
+    keepLooping = False
+    print("Goodbye")
